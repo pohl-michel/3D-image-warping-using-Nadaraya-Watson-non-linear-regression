985ec6168b080304db27fed837f493a7807cbc0c
Pohl Michel <64617547+pohl-michel@users.noreply.github.com>
2022-09-19 16:02:07 +0900

    Corrected error in documentation

--- load_3Dwarp_par.m
+++ load_3Dwarp_par.m
@@ -1,32 +1,32 @@
 function [ warp_par ] = load_3Dwarp_par()
-% The structure beh_par contains information about the parameters used for warping the image.
+% Returns the structure warp_par, which contains information about the parameters used for warping the image.
 %
 % Author : Pohl Michel
 % Date : August 31st, 2020
 % Version : v1.0
 % License : 3-clause BSD License
 
-warp_par.kernel_idx = 1;
-    % 1 = gaussian kernel
-    % 2 = averaging kernel
+    warp_par.kernel_idx = 1;
+        % 1 = gaussian kernel
+        % 2 = averaging kernel
 
-    switch(warp_par.kernel_idx)
-        case 1
-           warp_par.kernel_str = 'gaussian kernel';
-        case 2
-           warp_par.kernel_str = 'averaging kernel';
-    end
-    
-warp_par.kernel_appl_meth = 1;
-    % 1 = matrix computation
-    % 2 = pointwise computation
-    
-% standard deviation in the case of a gaussian kernel
-warp_par.sg_fw_wrp = 0.5; 
-    
-warp_par.min_filter_dim = 3;
-    % integer value
-    % if this value is too low, black pixels may occer in the warped image
+        switch(warp_par.kernel_idx)
+            case 1
+               warp_par.kernel_str = 'gaussian kernel';
+            case 2
+               warp_par.kernel_str = 'averaging kernel';
+        end
+
+    warp_par.kernel_appl_meth = 1;
+        % 1 = matrix computation
+        % 2 = pointwise computation
+
+    % standard deviation in the case of a gaussian kernel
+    warp_par.sg_fw_wrp = 0.5; 
+
+    warp_par.min_filter_dim = 3;
+        % integer value
+        % if this value is too low, black pixels may occer in the warped image
     
     % filter_dim : half size of the square filter
     switch(warp_par.kernel_idx)
